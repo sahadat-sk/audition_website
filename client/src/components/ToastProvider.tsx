986f8cdeba73191059e83ce6c20e56f1e92b8dde
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2023-05-08 10:20:54 +0530

    [FEAT] CUSTOM THEME COLORS IN REACT TOASTIFY

--- client/src/components/ToastProvider.tsx
+++ client/src/components/ToastProvider.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { ToastContainer, Zoom } from 'react-toastify';
+import { ToastContainer, Zoom, ToastClassName } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 type Props = {
   children: ReactNode;
+};
+
+const contextClass = {
+  success: 'bg-green text-onGreen dark:bg-greenDark dark:text-onGreenDark',
+  error: 'bg-red text-onRed dark:bg-redDark dark:text-onRedDark',
+  info: 'bg-gray-600',
+  warning: 'bg-orange-400',
+  default: 'bg-bg text-onBg dark:bg-bgDark dark:text-onBgDark',
 };
 
 export default function ToastProvider({ children }: Props) {
@@ -15,7 +23,10 @@ export default function ToastProvider({ children }: Props) {
       <ToastContainer
         autoClose={2000}
         hideProgressBar
-        toastClassName={'bg-red dark:bg-redDark text-onRed dark:text-onRedDark'}
+        toastClassName={(type) =>
+          contextClass[type?.type || 'default'] +
+          ' relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer'
+        }
         icon={''}
       />
     </>
